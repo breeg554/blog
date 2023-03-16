7cd493c7439646ed4ff43f990a2243758bc33530
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-03-16 22:12:58 +0100

    Setup fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-roboto)', ...fontFamily.sans],
+        serif: ['var(--font-merri)', ...fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
