df11498cf0fb83650f922f2477f0173234a8ddb9
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-05-26 20:16:31 +0200

    Add simple theme animation

--- components/client/ThemeSwitcher/ThemeSwitcher.tsx
+++ components/client/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,6 +1,8 @@
 'use client';
-import { FC, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { Theme } from '@components/types';
+import classNames from 'classnames';
+import { MoonSvg, SunSvg } from '@svgs';
 
 interface Props {
   theme: Theme;
@@ -21,5 +23,32 @@ export const ThemeSwitcher: FC<Props> = ({ theme }) => {
     }
   };
 
-  return <button onClick={toggleTheme}>{_theme === Theme.dark ? 'light' : 'dark'}</button>;
+  return (
+    <button
+      onClick={toggleTheme}
+      className={classNames(
+        'relative overflow-hidden rounded-xl w-10 h-10 p-2 bg-neutral-200 dark:bg-zinc-200 text-neutral-800 hover:text-blue-500 hover:text-blue-500',
+      )}
+    >
+      <MoonSvg
+        className={classNames(
+          'absolute top-1/2 left-1/2 -translate-x-1/2 transition duration-300',
+          {
+            '-translate-y-1/2 opacity-100': _theme === 'dark',
+            'translate-y-7 opacity-0': _theme === 'light',
+          },
+        )}
+      />
+
+      <SunSvg
+        className={classNames(
+          'absolute top-1/2 left-1/2 -translate-x-1/2 transition duration-300',
+          {
+            '-translate-y-1/2 opacity-100': _theme === 'light',
+            'translate-y-7 opacity-0': _theme === 'dark',
+          },
+        )}
+      />
+    </button>
+  );
 };
--- svgs/index.ts
+++ svgs/index.ts
@@ -1,3 +1,5 @@
 export * from './TwitterSvg';
 export * from './InSvg';
 export * from './GithubIcon';
+export * from './MoonSvg';
+export * from './SunSvg';
