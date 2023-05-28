64ea03dcae16bd715e1a774cf541fbf9f5662343
Dawid Kiełbasa <dkielbasa@topsa.com.pl>
2023-05-28 12:37:12 +0200

    Update post card

--- components/PostCard/PostCard.tsx
+++ components/PostCard/PostCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import Link from 'next/link';
 import { Post } from '@lib/posts';
 import SocialLinks from './SocialLinks';
-import Link from 'next/link';
 
 interface PostCardProps {
   post: Post;
@@ -9,21 +9,26 @@ interface PostCardProps {
 
 export const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
-    <article className="border-b border-neutral-200 py-4 md:py-6">
-      <Link href={post.slug} className="block mb-1">
+    <article className="py-4 md:py-6">
+      <Link href={post.slug} className="block mb-3">
         <h3 className="text-2xl text-neutral-900 dark:text-white font-bold font-sans line-clamp-3 underline hover:no-underline hover:text-blue-500 hover:dark:text-blue-500 md:text-3xl">
           {post.title}
         </h3>
       </Link>
 
-      <p className="text-xs text-neutral-600 dark:text-neutral-300 mb-2 ">
-        {post.date} &#x2022; <span>{post.readIn}</span>
-      </p>
-      <p className="text-sm text-neutral-700 dark:text-neutral-300 line-clamp-2 mb-1 md:text-base">
+      <p className="text-sm text-neutral-700 dark:text-neutral-300 line-clamp-2 mb-3 md:text-base">
         {post.description}
       </p>
 
-      <div className="mt-3 w-full flex justify-between">
+      <div className="text-xs text-neutral-700 dark:text-neutral-300 flex items-center gap-3">
+        <p>{post.date}</p>
+        <p>{post.readIn}</p>
+        <p>312 views</p>
+      </div>
+
+      <div className="w-full border-b border-neutral-200 mt-4 mb-2" />
+
+      <div className="w-full flex justify-between">
         <SocialLinks post={post} />
 
         <Link href={post.slug} className="text-xs font-bold text-neutral-900 dark:text-white">
