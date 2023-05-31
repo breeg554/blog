3535c60c47f5b168cbf6a294af53ce3c408c1c8b
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-06-01 00:21:12 +0200

    Update social links

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,7 @@
 export const socials = {
-  github: 'https://gh.com',
-  linkedin: 'https://linkedin.com',
-  twitter: 'https://twitter.com',
+  github: 'https://github.com/breeg554',
+  linkedin: 'https://www.linkedin.com/in/dawid-kie%C5%82basa-18b6a3198/',
+  twitter: 'https://twitter.com/breeg554',
 };
 
 export const domain = 'https://frontendcorner.dev';
