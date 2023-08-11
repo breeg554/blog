1c9555548af07f47a09c407c0d001b0752a6b074
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2023-08-11 18:35:33 +0200

    Update description

--- components/Author/Author.tsx
+++ components/Author/Author.tsx
@@ -11,13 +11,15 @@ export const Author: React.FC<AuthorProps> = ({ size = 'lg' }) => {
     <section>
       <div className="flex flex-col gap-3 sm:flex-row">
         <Avatar src="/me.webp" alt="Dawid Kiełbasa" size={size} />
-        <h1 className="text-neutral-700 dark:text-neutral-300 text-sm max-w-lg md:text-base">
-          <span className="text-neutral-900 font-bold text-xl">Hi there 👋</span>{' '}
-          <span className="block">
+        <div>
+          <h1 className="text-neutral-700 dark:text-neutral-300 text-smmd:text-base">
+            <span className="text-neutral-900 font-bold text-xl">Hi there 👋</span>
+          </h1>
+          <p className="max-w-lg text-neutral-700 dark:text-neutral-300">
             Dawid Kielbasa here. Welcome to my corner of frontend web! Stay a while and see what
             I&apos;ve been working on.
-          </span>
-        </h1>
+          </p>
+        </div>
       </div>
     </section>
   );
